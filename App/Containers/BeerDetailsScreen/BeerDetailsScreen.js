2c91848cd633d50e496584adf2a076e1c03cfb51
mkpietrzyk <mkm.pietrzyk@gmail.com>
2017-12-28 17:11:09 +0100

    adding utility elements, creating files hierarchy

---
+++ App/Containers/BeerDetailsScreen/BeerDetailsScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {connect, provider} from 'react-redux';
+
+
+export default class BeerDetailsScreen extends Component() {
+    render() {
+        return (
+            <View>
+                <Text> This is Beer details Screen </Text>
+            </View>
+        );
+    }
+}
---
+++ App/Theme/Colors.js
@@ -0,0 +1,6 @@
+const colors = {
+    primary : '#FDE3A7', // Cape Honey
+    secondary : '#F4B350', // Casablanca
+}
+
+export default colors;
