c98a9cb24b071dc9e21cb01ffe6cf2ecccdf7b7e
mkpietrzyk <mkm.pietrzyk@gmail.com>
2017-12-29 02:37:54 +0100

    main App file cleanup

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -4,35 +4,14 @@ import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import store from '../Redux/store';
 import AppNavigator from '../Navigation/AppNavigator'
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component<{}> {
     render() {
         return (
             <Provider store={store}>
-                <View style={styles.container}>
                     <AppNavigator/>
-                </View>
             </Provider>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-})
 
